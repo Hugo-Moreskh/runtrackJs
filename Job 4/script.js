9d3fc1622f0343281670c4d20d055015cb1edca1
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-03-26 10:24:10 +0100

    changement de fichier jour 1/2

--- Job 4/script.js
+++
@@ -1,9 +0,0 @@
-function bisextile(annee) {
-  if ((annee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-annee = 2024;
-console.log(bisextile(annee));
