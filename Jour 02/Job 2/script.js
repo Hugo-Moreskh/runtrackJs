35c318c35115eca4bc834bd06809f4c589fd6e9a
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-03-26 11:29:02 +0100

    ajout du job 2

---
+++ Jour 02/Job 2/script.js
@@ -0,0 +1,12 @@
+function showhide() {
+  var article = document.getElementById("citation");
+  if (article.style.display === "none") {
+    article.style.display = "block";
+  } else {
+    article.style.display = "none";
+  }
+}
+
+document.getElementById("button").addEventListener("click", function () {
+  showhide();
+});
